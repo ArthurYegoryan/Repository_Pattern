2957a47c4c86050badbe8e3132cf2a51cbcda4a2
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2022-06-01 17:56:29 +0400

    Added new project

---
+++ Repository_Pattern/DataAccessEF/TypeRepository/AddressRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Interfaces;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessEF.TypeRepository
+{
+    public class AddressRepository : GenericRepository<Address>, IAddressRepository
+    {
+        public AddressRepository(PeopleContext context) : base(context) { }
+    }
+}
---
+++ Repository_Pattern/WebAPI/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+using Domain;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public PersonController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> GetAllPersons()
+        {
+            return unitOfWork.Person.GetAll();
+        }
+
+        [Route("[action]")]
+        [HttpGet]
+        public IEnumerable<Person> GetAdultPersons()
+        {
+            return unitOfWork.Person.GetAdultPersons();
+        }
+    }
+}
